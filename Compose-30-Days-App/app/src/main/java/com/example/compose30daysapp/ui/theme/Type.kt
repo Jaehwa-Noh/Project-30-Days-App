ea84b99b3c1cd8ee644aaef4c22041fc560f55c1
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-29 15:23:35 +0900

    Remove comment.

--- Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/ui/theme/Type.kt
+++ Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/ui/theme/Type.kt
@@ -16,7 +16,6 @@ val EastSeaDokdo = FontFamily(
     Font(R.font.east_sea_dokdo_regular, FontWeight.Normal)
 )
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyMedium = TextStyle(
         fontFamily = NanumGothic,
@@ -33,20 +32,4 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
