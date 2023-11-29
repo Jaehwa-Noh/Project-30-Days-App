22259969cc951e1bf1f17c124a3a0aab028e3989
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-29 15:23:11 +0900

    Set fontFamily and font.

--- Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/ui/theme/Type.kt
+++ Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/ui/theme/Type.kt
@@ -2,18 +2,36 @@ package com.example.compose30daysapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.compose30daysapp.R
+
+val NanumGothic = FontFamily(
+    Font(R.font.nanum_gothic_regular, FontWeight.Normal)
+)
+
+val EastSeaDokdo = FontFamily(
+    Font(R.font.east_sea_dokdo_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = NanumGothic,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = EastSeaDokdo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = EastSeaDokdo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
