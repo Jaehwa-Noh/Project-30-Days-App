db3d89f87f68c9a3a13215a01d3a5b9c29d72d38
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-30 15:26:40 +0900

    Add drawable property at the BookContent Model.

--- Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/data/BookContent.kt
+++ Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/data/BookContent.kt
@@ -1,9 +1,11 @@
 package com.example.compose30daysapp.data
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class BookContent(
     @StringRes val titleStringId: Int,
     @StringRes val contentStringId: Int,
-    @StringRes val sourceStringId: Int
+    @StringRes val sourceStringId: Int,
+    @DrawableRes val imageId: Int
 )
--- Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/model/BookContentsRepository.kt
+++ Compose-30-Days-App/app/src/main/java/com/example/compose30daysapp/model/BookContentsRepository.kt
@@ -5,35 +5,185 @@ import com.example.compose30daysapp.data.BookContent
 
 object BookContentsRepository {
     val bookContents = listOf(
-        BookContent(R.string.day1_title, R.string.day1_content, R.string.day1_source),
-        BookContent(R.string.day2_title, R.string.day2_content, R.string.day2_source),
-        BookContent(R.string.day3_title, R.string.day3_content, R.string.day3_source),
-        BookContent(R.string.day4_title, R.string.day4_content, R.string.day4_source),
-        BookContent(R.string.day5_title, R.string.day5_content, R.string.day5_source),
-        BookContent(R.string.day6_title, R.string.day6_content, R.string.day6_source),
-        BookContent(R.string.day7_title, R.string.day7_content, R.string.day7_source),
-        BookContent(R.string.day8_title, R.string.day8_content, R.string.day8_source),
-        BookContent(R.string.day9_title, R.string.day9_content, R.string.day9_source),
-        BookContent(R.string.day10_title, R.string.day10_content, R.string.day10_source),
-        BookContent(R.string.day11_title, R.string.day11_content, R.string.day11_source),
-        BookContent(R.string.day12_title, R.string.day12_content, R.string.day12_source),
-        BookContent(R.string.day13_title, R.string.day13_content, R.string.day13_source),
-        BookContent(R.string.day14_title, R.string.day14_content, R.string.day14_source),
-        BookContent(R.string.day15_title, R.string.day15_content, R.string.day15_source),
-        BookContent(R.string.day16_title, R.string.day16_content, R.string.day16_source),
-        BookContent(R.string.day17_title, R.string.day17_content, R.string.day17_source),
-        BookContent(R.string.day18_title, R.string.day18_content, R.string.day18_source),
-        BookContent(R.string.day19_title, R.string.day19_content, R.string.day19_source),
-        BookContent(R.string.day20_title, R.string.day20_content, R.string.day20_source),
-        BookContent(R.string.day21_title, R.string.day21_content, R.string.day21_source),
-        BookContent(R.string.day22_title, R.string.day22_content, R.string.day22_source),
-        BookContent(R.string.day23_title, R.string.day23_content, R.string.day23_source),
-        BookContent(R.string.day24_title, R.string.day24_content, R.string.day24_source),
-        BookContent(R.string.day25_title, R.string.day25_content, R.string.day25_source),
-        BookContent(R.string.day26_title, R.string.day26_content, R.string.day26_source),
-        BookContent(R.string.day27_title, R.string.day27_content, R.string.day27_source),
-        BookContent(R.string.day28_title, R.string.day28_content, R.string.day28_source),
-        BookContent(R.string.day29_title, R.string.day29_content, R.string.day29_source),
-        BookContent(R.string.day30_title, R.string.day30_content, R.string.day30_source)
+        BookContent(
+            R.string.day1_title,
+            R.string.day1_content,
+            R.string.day1_source,
+            R.drawable.day1
+        ),
+        BookContent(
+            R.string.day2_title,
+            R.string.day2_content,
+            R.string.day2_source,
+            R.drawable.day2
+        ),
+        BookContent(
+            R.string.day3_title,
+            R.string.day3_content,
+            R.string.day3_source,
+            R.drawable.day3
+        ),
+        BookContent(
+            R.string.day4_title,
+            R.string.day4_content,
+            R.string.day4_source,
+            R.drawable.day4
+        ),
+        BookContent(
+            R.string.day5_title,
+            R.string.day5_content,
+            R.string.day5_source,
+            R.drawable.day5
+        ),
+        BookContent(
+            R.string.day6_title,
+            R.string.day6_content,
+            R.string.day6_source,
+            R.drawable.day6
+        ),
+        BookContent(
+            R.string.day7_title,
+            R.string.day7_content,
+            R.string.day7_source,
+            R.drawable.day7
+        ),
+        BookContent(
+            R.string.day8_title,
+            R.string.day8_content,
+            R.string.day8_source,
+            R.drawable.day8
+        ),
+        BookContent(
+            R.string.day9_title,
+            R.string.day9_content,
+            R.string.day9_source,
+            R.drawable.day9
+        ),
+        BookContent(
+            R.string.day10_title,
+            R.string.day10_content,
+            R.string.day10_source,
+            R.drawable.day10
+        ),
+        BookContent(
+            R.string.day11_title,
+            R.string.day11_content,
+            R.string.day11_source,
+            R.drawable.day11
+        ),
+        BookContent(
+            R.string.day12_title,
+            R.string.day12_content,
+            R.string.day12_source,
+            R.drawable.day12
+        ),
+        BookContent(
+            R.string.day13_title,
+            R.string.day13_content,
+            R.string.day13_source,
+            R.drawable.day13
+        ),
+        BookContent(
+            R.string.day14_title,
+            R.string.day14_content,
+            R.string.day14_source,
+            R.drawable.day14
+        ),
+        BookContent(
+            R.string.day15_title,
+            R.string.day15_content,
+            R.string.day15_source,
+            R.drawable.day15
+        ),
+        BookContent(
+            R.string.day16_title,
+            R.string.day16_content,
+            R.string.day16_source,
+            R.drawable.day16
+        ),
+        BookContent(
+            R.string.day17_title,
+            R.string.day17_content,
+            R.string.day17_source,
+            R.drawable.day17
+        ),
+        BookContent(
+            R.string.day18_title,
+            R.string.day18_content,
+            R.string.day18_source,
+            R.drawable.day18
+        ),
+        BookContent(
+            R.string.day19_title,
+            R.string.day19_content,
+            R.string.day19_source,
+            R.drawable.day19
+        ),
+        BookContent(
+            R.string.day20_title,
+            R.string.day20_content,
+            R.string.day20_source,
+            R.drawable.day20
+        ),
+        BookContent(
+            R.string.day21_title,
+            R.string.day21_content,
+            R.string.day21_source,
+            R.drawable.day21
+        ),
+        BookContent(
+            R.string.day22_title,
+            R.string.day22_content,
+            R.string.day22_source,
+            R.drawable.day22
+        ),
+        BookContent(
+            R.string.day23_title,
+            R.string.day23_content,
+            R.string.day23_source,
+            R.drawable.day23
+        ),
+        BookContent(
+            R.string.day24_title,
+            R.string.day24_content,
+            R.string.day24_source,
+            R.drawable.day24
+        ),
+        BookContent(
+            R.string.day25_title,
+            R.string.day25_content,
+            R.string.day25_source,
+            R.drawable.day25
+        ),
+        BookContent(
+            R.string.day26_title,
+            R.string.day26_content,
+            R.string.day26_source,
+            R.drawable.day26
+        ),
+        BookContent(
+            R.string.day27_title,
+            R.string.day27_content,
+            R.string.day27_source,
+            R.drawable.day27
+        ),
+        BookContent(
+            R.string.day28_title,
+            R.string.day28_content,
+            R.string.day28_source,
+            R.drawable.day28
+        ),
+        BookContent(
+            R.string.day29_title,
+            R.string.day29_content,
+            R.string.day29_source,
+            R.drawable.day29
+        ),
+        BookContent(
+            R.string.day30_title,
+            R.string.day30_content,
+            R.string.day30_source,
+            R.drawable.day30
+        )
     )
 }
