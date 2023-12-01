54c47bb57de5d14949b3801a9683114cdaa085af
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-01 16:43:04 +0900

    Init Project.

---
+++ SwiftUI-30-Days-App/SwiftUI-30-Days-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-30-Days-App
+//
+//  Created by Jaehwa Noh on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
