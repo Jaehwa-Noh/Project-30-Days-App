262160f709c2be63c7c22fa7a111efb9cb07c666
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-03 11:16:48 +0900

    Create Colors enum for color asset.

---
+++ SwiftUI-30-Days-App/SwiftUI-30-Days-App/Supporting Files/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  SwiftUI-30-Days-App
+//
+//  Created by Jaehwa Noh on 12/3/23.
+//
+
+import Foundation
+
+enum Colors: String {
+    case backgroundColor = "BackgroundColor"
+    case cardBackgroundColor = "CardBackgroundColor"
+}
